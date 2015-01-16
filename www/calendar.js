eb94d532c802da85da30e78d471af7d7388bf510
fachamix <fachamix@hotmail.com>
2015-01-16 20:56:31 -0300

    Primer pulso

---
+++ www/calendar.js
@@ -0,0 +1,19 @@
+var calendar = {
+	createEvent: function (title,location,notes,startDate,endDate,successCallback,errorCallback) {
+		cordova.exec(
+			successCallback,
+			errorCallback,
+			'Calendar',
+			'addCalendarEntry',
+			[{
+				"title":title,
+				"description":notes,
+				"eventLocation":location,
+				"startTimeMillis":startDate.getTime(),
+				"endTimeMillis":endDate.getTime()
+			}]
+		);
+	}
+}
+
+module.exports = calendar;
